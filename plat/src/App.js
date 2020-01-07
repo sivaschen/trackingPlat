9bf95d5a297ee0abea9802df5b993867df38bcad
Chen Cheng <oncwnuNuUVxFWPeMcDX8XnuR9XiI@git.weixin.qq.com>
2020-01-07 11:44:14 +0800

    test git save pwd and username

--- plat/src/App.js
+++ plat/src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React test git save username and pwd
         </a>
       <Router />
       </header>
