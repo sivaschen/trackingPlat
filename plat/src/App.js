c951128600e0b4010bdc3f0f3d38fd1b7745e43e
Chen Cheng <oncwnuNuUVxFWPeMcDX8XnuR9XiI@git.weixin.qq.com>
2020-01-10 19:22:08 +0800

    loginPage

--- plat/src/App.js
+++ plat/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Router from './router'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       <Router />
-      </header>
     </div>
   );
 }
--- plat/src/component/login/login.js
+++ plat/src/component/login/login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Input } from 'antd';
+import { Input, Icon } from 'antd';
+import "./login.scss"
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="login">
+      <div className="loginBox">
         <div className="header">
           <img alt=""></img>
         </div>
@@ -12,8 +13,9 @@ export default class Home extends Component {
           <div className="left">
 
           </div>
-          <div className="loginBox">
-            <Input placeholder="Basic usage" />
+          <div className="loginInput">
+            <Input addonBefore="账号" placeholder="请输入用户名" />
+            <Input.Password addonBefore="密码" placeholder="请输入密码"/>
           </div>
         </div>
       </div>
