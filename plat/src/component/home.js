054e4683c38e2cb32f3417880d5d9aedc39e1420
Chen Cheng <oncwnuNuUVxFWPeMcDX8XnuR9XiI@git.weixin.qq.com>
2020-01-15 19:45:06 +0800

    login finish

--- plat/src/component/home.js
+++ plat/src/component/home.js
@@ -13,7 +13,7 @@ export default class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      collapsed: false
+      current: 'mail'
     }
   }
   componentDidMount() {
@@ -21,54 +21,52 @@ export default class Home extends Component {
   onSelect = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
   };
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+  logout = () => {
+    this.props.history.push("/login");
+  }
   render() {
     return (
       <div className="home">
-        <header>asdfasdfasdfasdfsaef</header>
+        <header>asdfasdfasdfasdfsaef <Button onClick={this.logout}>退 出</Button></header>
         <div className="menu">
-          <Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="horizontal" theme="dark"
-            inlineCollapsed={this.state.collapsed} style={{paddingLeft: 200}}>
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="inbox" />
-              <span>Option 3</span>
-            </Menu.Item>
-            <SubMenu key="sub1"
-              title={
-                <span>
-                  <Icon type="mail" />
-                  <span>Navigation One</span>
-                </span>
-              }
-            >
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <Menu.Item key="7">Option 7</Menu.Item>
-              <Menu.Item key="8">Option 8</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2"
-              title={
-                <span>
-                  <Icon type="appstore" />
-                  <span>Navigation Two</span>
-                </span>
-              }
-            >
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="11">Option 11</Menu.Item>
-                <Menu.Item key="12">Option 12</Menu.Item>
-              </SubMenu>
-            </SubMenu>
-          </Menu>
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+          <Menu.Item key="mail">
+            <Icon type="mail" />
+            Navigation One
+          </Menu.Item>
+          <Menu.Item key="app" disabled>
+            <Icon type="appstore" />
+            Navigation Two
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span className="submenu-title-wrapper">
+                <Icon type="setting" />
+                Navigation Three - Submenu
+              </span>
+            }
+          >
+            <Menu.ItemGroup title="Item 1">
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Item 2">
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <Menu.Item key="alipay">
+            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+              Navigation Four - Link
+            </a>
+          </Menu.Item>
+        </Menu>
         </div>
      
         <div className="tree">
--- plat/src/component/login/login.js
+++ plat/src/component/login/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Icon, Button } from 'antd';
+import { Input, Icon, Button, message  } from 'antd';
 import "./login.scss"
 import http from "./../server"
 import md5 from 'md5'
@@ -8,13 +8,35 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      userName: "",
+      pwd: ""
     }
   };
+  setUserName = (e) => {
+    this.setState({
+      userName: e.target.value
+    })
+  }
+  setPwd = (e) => {
+    this.setState({
+      pwd: e.target.value
+    })
+  }
   loginSys = () => {
-    var time = Math.floor(new Date().getTime() / 1000);
-    var url = "/api" + "login/loginByEnt?login_name=rayjay&time=" + time + "&signature=" + md5(md5("666666") + time)
+    const {userName, pwd} = this.state;
+    if (!userName || !pwd) {
+      message.warning("请输入用户名和密码");
+      return
+    }
+    let time = Math.floor(new Date().getTime() / 1000);
+    let url = "/api" + "login/loginByEnt?login_name=" + userName +"&time=" + time + "&signature=" + md5(md5(pwd) + time);
     http.get(url).then(res => {
-      // this.props.history.push('/home');
+      if (res.data.errcode === 0) {
+        let targetUrl = res.data.data.url;
+        this.props.history.push(targetUrl);
+      } else if (res.data.errcode === 3) {
+        message.error('密码错误');
+      }
     })
   };
   render() {
@@ -30,8 +52,8 @@ export default class Home extends Component {
           <div className="loginInput">
             {/* <h3>定位平台</h3> */}
             <h3>XXXX</h3>
-            <Input addonBefore="账号" placeholder="请输入用户名" />
-            <Input.Password addonBefore="密码" placeholder="请输入密码" className="password"/>
+            <Input addonBefore="账号" placeholder="请输入用户名" value={this.state.userName} onChange={this.setUserName}/>
+            <Input.Password addonBefore="密码" placeholder="请输入密码" className="password" value={this.state.pwd} onChange={this.setPwd.bind(this)}/>
             <Button type="primary" onClick={this.loginSys}>登 录<Icon type="login" /></Button>
           </div>
         </div>
