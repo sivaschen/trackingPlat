a7a50d63014155dcd032c133e43ba99215ec3485
cc <417273365@qq.com>
2021-08-04 21:39:08 +0800

    退回版本

--- plat/src/setupProxy.js
+++ plat/src/setupProxy.js
@@ -4,7 +4,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
   app.use(
     proxy('/api', {  //`api`是需要转发的请求 
-      target: 'http://webbo.yunjiwulian.com',  // 这里是接口服务器地址
+      target: 'http://47.106.201.46',  // 这里是接口服务器地址
       changeOrigin: true,
       ws: true, // proxy websockets
       pathRewrite: {
